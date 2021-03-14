8191a04019ca0053536f85a73c4caa4468c8516a
MatveyMA <matveym2048@gmail.com>
2021-03-14 03:39:10 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+let result;
+let mod = Math.abs(n);
+let str = String(mod);
+splitstr = str.split("");
+reversed = splitstr.reverse();
+result = reversed.join(""); 
+return Number(result);
 }
